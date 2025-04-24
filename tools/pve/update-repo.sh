82be7f3333180e6616c8113a0fc7a7df192b0871
Lutzi1112 <49982925+Lutzi1112@users.noreply.github.com>
2025-04-24 23:50:15 +0200

    1

--- tools/pve/update-repo.sh
+++ tools/pve/update-repo.sh
@@ -1,9 +1,9 @@
-Lutzi1112/ProxmoxVE#!/usr/bin/env bash
+Lutzi1112/ProxmoxVEVE#!/usr/bin/env bash
 
 # Copyright (c) 2021-2025 community-scripts
 # Author: MickLesk
 # License: MIT
-# https://github.com/Lutzi1112/ProxmoxVE/raw/main/LICENSE
+# https://github.com/Lutzi1112/ProxmoxVEVE/raw/main/LICENSE
 
 function header_info {
   clear
@@ -35,14 +35,14 @@ function update_container() {
     echo -e "${BL}[Info]${GN} Checking /usr/bin/update in ${BL}$container${CL} (OS: ${GN}$os${CL})"
 
     if pct exec "$container" -- [ -e /usr/bin/update ]; then
-      if pct exec "$container" -- grep -q "Lutzi1112/Proxmox" /usr/bin/update; then
+      if pct exec "$container" -- grep -q "Lutzi1112/ProxmoxVE" /usr/bin/update; then
         echo -e "${RD}[No Change]${CL} /usr/bin/update is already up to date in ${BL}$container${CL}.\n"
-      elif pct exec "$container" -- grep -q -v "Lutzi1112/ProxmoxVE" /usr/bin/update; then
-        echo -e "${RD}[Warning]${CL} /usr/bin/update in ${BL}$container${CL} contains a different entry (${RD}Lutzi1112/ProxmoxVE${CL}). No changes made.\n"
+      elif pct exec "$container" -- grep -q -v "Lutzi1112/ProxmoxVEVE" /usr/bin/update; then
+        echo -e "${RD}[Warning]${CL} /usr/bin/update in ${BL}$container${CL} contains a different entry (${RD}Lutzi1112/ProxmoxVEVE${CL}). No changes made.\n"
       else
-        pct exec "$container" -- bash -c "sed -i 's/Lutzi1112/ProxmoxVE\\/Proxmox/Lutzi1112\\/Proxmox/g' /usr/bin/update"
+        pct exec "$container" -- bash -c "sed -i 's/Lutzi1112/ProxmoxVEVE\\/Proxmox/Lutzi1112\\/ProxmoxVE/g' /usr/bin/update"
 
-        if pct exec "$container" -- grep -q "Lutzi1112/Proxmox" /usr/bin/update; then
+        if pct exec "$container" -- grep -q "Lutzi1112/ProxmoxVE" /usr/bin/update; then
           echo -e "${GN}[Success]${CL} /usr/bin/update updated in ${BL}$container${CL}.\n"
         else
           echo -e "${RD}[Error]${CL} /usr/bin/update in ${BL}$container${CL} could not be updated properly.\n"
